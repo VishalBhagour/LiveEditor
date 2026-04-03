942c331f426e88cb1db5d4caa06ff16bc2a561bd
yashchaudhary1605 <chaudharyyash64036@gmail.com>
2026-04-03 12:52:24 +0530

    [YashChaudhary]add:-Client in component

--- client/live-editor/src/components/Client.js
+++ client/live-editor/src/components/Client.js
@@ -5,7 +5,9 @@ const Client = ({ username }) => {
     return (
         <div className="client">
             <Avatar name={username} size={50} round="14px" />
+
             <span className="userName">{username}</span>
+            
         </div>
     );
 };
